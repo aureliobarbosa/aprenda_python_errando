745586931680bdc04c1453bbcee172164be76d57
Marco Aurélio A. Barbosa <aureliobarbosa@gmail.com>
2023-05-27 11:25:52 -0300

    Módulo 4: Exemplo do Eco (while)

---
+++ modulo4/eco.py
@@ -0,0 +1,14 @@
+# Página. 111
+
+print("Eco. Um programa que te repete!\n"
+      "Para sair digite cansei, como abaixo:\n"
+      "> cansei\n\n"
+      )
+
+while True:
+    entrada = input("> ")
+    if entrada == 'cansei':
+        break
+    print(entrada, end='\n\n')
+
+print("Eu também cansei! Até mais.\n")
